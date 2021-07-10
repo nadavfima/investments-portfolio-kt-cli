539ca0cd25151a72d17aa3fe1d8c7ed69a0814ca
Nadav Fima <nadavfima@gmail.com>
2021-07-10 15:20:50 +0300

    1.0.1 actions fix

--- build.gradle.kts
+++ build.gradle.kts
@@ -8,8 +8,8 @@ subprojects {
     tasks.register<Copy>("packageDistribution") {
         dependsOn("jar")
         from("${project.rootDir}/scripts/portfolio-cli")
-        from("${project.projectDir}/build/libs/${project.name}-${project.version}.jar") {
-            into("lib/${project.name}.jar")
+        from("${project.projectDir}/build/libs/") {
+            into("lib/")
         }
         into("${project.rootDir}/dist")
     }
