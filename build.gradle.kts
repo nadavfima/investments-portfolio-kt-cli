36c8ed4a741420d20bcd295689701809164b0bd2
Nadav Fima <nadavfima@gmail.com>
2021-07-10 15:16:58 +0300

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -8,8 +8,8 @@ subprojects {
     tasks.register<Copy>("packageDistribution") {
         dependsOn("jar")
         from("${project.rootDir}/scripts/portfolio-cli")
-        from("${project.projectDir}/build/libs/${project.name}.jar") {
-            into("lib")
+        from("${project.projectDir}/build/libs/${project.name}-${project.version}.jar") {
+            into("lib/${project.name}.jar")
         }
         into("${project.rootDir}/dist")
     }
