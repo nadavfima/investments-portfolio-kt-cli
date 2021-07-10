7416adb5e56633f0f6d1530024f5bdfe7783a5ba
Nadav Fima <nadavfima@gmail.com>
2021-07-10 14:31:00 +0300

    fixed missing slf4j-simple

--- portfolio-cli-app/build.gradle.kts
+++ portfolio-cli-app/build.gradle.kts
@@ -49,6 +49,9 @@ dependencies {
     // Use Webdriver Manager for Selenium
     implementation("io.github.bonigarcia", "webdrivermanager", "4.4.3")
 
+    implementation("org.slf4j", "slf4j-api", "1.7.30")
+    implementation("org.slf4j", "slf4j-simple", "1.7.30")
+
 }
 
 application {
